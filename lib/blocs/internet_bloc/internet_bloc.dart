99b0ef5db44e0d618bda934b77da83d6b41f090a
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-03-10 21:59:31 +0530

    creating bloc

---
+++ lib/blocs/internet_bloc/internet_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:block_pattern/blocs/internet_bloc/internet_event.dart';
+import 'package:block_pattern/blocs/internet_bloc/internet_state.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class InternetBloc extends Bloc<InternetEvent, InternetState> {
+  final Connectivity _connectivity = Connectivity();
+
+  InternetBloc() : super(InternetInitialState()) {
+    on<InternetLostEvent>((event, emit) => emit(InternetLostState()));
+    on<InternetGainEvent>((event, emit) => emit(InternetGainState()));
+
+    _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        add(InternetGainEvent());
+      } else {
+        add(InternetLostEvent());
+      }
+    });
+  }
+}
---
+++ lib/blocs/internet_bloc/internet_state.dart
@@ -0,0 +1,15 @@
+abstract class InternetState{
+
+}
+
+class InternetInitialState extends InternetState{
+
+}
+
+class InternetGainState extends InternetState{
+
+}
+
+class InternetLostState extends InternetState{
+  
+}
