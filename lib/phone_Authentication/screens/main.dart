e0d574d0fe75d0952842ef0b5392c98283e682c5
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-07-13 01:48:06 +0530

    connected_with_firebaseClI

--- lib/phone_Authentication/screens/main.dart
+++ lib/phone_Authentication/screens/main.dart
@@ -1,9 +1,16 @@
+import 'package:block_pattern/firebase_options.dart';
 import 'package:block_pattern/phone_Authentication/screens/sign_in.dart';
 import 'package:block_pattern/phone_Authentication/screens/verify_phone_number.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(MaterialApp(
     home: VerifyPhoneNumberScreen(),
   ));
